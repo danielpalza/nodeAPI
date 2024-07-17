554c8ecd044a5f02b8e3b8348f168f35829054f2
Daniel Palza <danielpalza@hotmail.com>
2024-07-16 21:08:52 -0300

    Create 'boards' controllers and routes

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./authRoutes.js"
+import boardRoutes from "./boardRoutes.js"
 
 const router = Router()
 
 router.use("/sessions", authRoutes)
+router.use("/boards", boardRoutes)
 
 
 export default router; 
