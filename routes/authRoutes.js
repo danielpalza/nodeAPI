dcd2b4b60e21e4b2d6577289dbbbf3c45049af04
Daniel Palza <danielpalza@hotmail.com>
2024-08-19 23:34:20 -0300

    fix name 'login' route to 'auth'

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,5 +10,5 @@ import createToken from "../middlewares/createToken.js"
 const router = Router()
 
 router.post("/register", accountExists, createHash, create);
-router.post("/login", userExists, verifyHash, createToken, login);
-export default router;
+router.post("/auth", userExists, verifyHash, createToken, login);
+export default router;  
